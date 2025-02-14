b1c781b902c67b0db24f3bdb5aaa63e71031bfc7
Nicholas Strzelczyk <17nas6@queensu.ca>
2025-02-14 13:00:55 -0500

    added new csv file with accuracy and f1 scores

---
+++ video_test_scripts/get_f1_scores.py
@@ -0,0 +1,34 @@
+import os
+import pandas as pd
+import numpy as np
+
+
+if __name__ == '__main__':
+
+    # open the main csv file
+    csv_path = "VideoResultsCSV.csv"
+    df = pd.read_csv(csv_path)
+
+    # calculate accuracy
+    accuracies = []
+    for index, row in df.iterrows():
+        tp = row['Detections']
+        total = row['Frames']
+        accuracy = 1.00 if tp > total else np.round(tp / total, 2)
+        accuracies.append(accuracy)
+    df['Accuracy'] = accuracies
+
+    # calculate the f1 scores
+    f1_scores = []
+    for index, row in df.iterrows():
+        tp = row['Detections']
+        fp = row['Frames'] - row['Detections'] if row['Detections'] > row['Frames'] else 0
+        fn = row['Frames'] - row['Detections'] if row['Detections'] < row['Frames'] else 0
+        f1_score = np.round(tp / (tp + (0.5 * (fp + fn))), 2)
+        f1_scores.append(f1_score)
+    df['F1Score'] = f1_scores
+
+    # save to a new csv file
+    print(df.head())
+    df.to_csv('VideoResultsCSV_WithMetrics.csv', index=False)
+
